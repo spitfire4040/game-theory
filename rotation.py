5fef406abb6dfe665a92bbba9f2a2f72a83084d8
Jay <spitfire4040@protonmail.com>
2019-04-17 16:58:29 -0700

    multi-player

--- rotation.py
+++ rotation.py
@@ -6,10 +6,12 @@ import pickle
 import matplotlib.pyplot as plt
 import csv
 
-players = [0,1,2,3,4,5,6,7,8,9]
+players = [0]
 val_array = []
 
 # initialize variables
+V1 = 0.0	# value of my current data
+V2 = 0.0	# average value of cybex data
 Sc = 0.0    # set initial value for Sc
 Sf = 0.0    # set initial value for Sf
 x = 0.01   	# set cost per GB for processing
@@ -24,7 +26,7 @@ c = 1		# cost charged by CYBEX for participation
 round = 0	# initialize round in game
 
 def calculate(I,s,q):
-	global round,Sc,Sf,Q,R,Ra,r,a,b,c,x
+	global round,Sc,Sf,Q,R,Ra,r,a,b,c,x,V1,V2
 
 	for x in range(0,10):
 		for player in players:
@@ -73,8 +75,8 @@ def calculate(I,s,q):
 			Sf += s
 
 			# calculate current value of CYBEX data for sharing (for the firm)
-			V2 = (b * (math.log(1 + (Sc * Q)))) - Ra   # this is the value of the data in CYBEX plus my data
-			V1 = (b * (math.log(1 + (s * q)))) - Ra    # this is the value of my data only
+			V2 = (b * (math.log(1 + (Sc * Q)))) - Ra   # this is the average value of the data in CYBEX
+			V1 = (b * (math.log(1 + (s * q)))) - Ra    # this is the value of the data I'm sharing now
 
 			# calculate sharing utility
 			if V2 - V1 < 0:
@@ -92,7 +94,8 @@ def calculate(I,s,q):
 				outfile.write(str(Sf) + '\n')
 
 			# save amount and quality of database to file as tuple list
-			val_array.append((round,player,I,s,q,Sc,Sf,Q,Ra,c,x))
+			#val_array.append((round,player,I,s,q,Sc,Sf,Q,Ra,c,x,V2,V1))
+			val_array.append((round,player,I,s,q,V2,V1))			
 			round += 1
 
 			with open('/home/jay/game-theory/cybex_value.txt', 'wb') as outfile:
@@ -100,12 +103,13 @@ def calculate(I,s,q):
 
 def graph():
 	# global variables
-	global round,player,I,s,q,Sc,Sf,Q,R,Ra,r1,r2,a,b,c,x
+	global round,player,I,s,q,Sc,Sf,Q,R,Ra,r1,r2,a,b,c,x,V1,V2
 
 	# create .csv file from data
 	with open('/home/jay/game-theory/cybex_value.csv', 'w') as csvFile:
 		writer = csv.writer(csvFile)
-		writer.writerow(("round","player","invest.","quantity","quality","q-cybex","q-firm","av-quality","av-risk","cost-cybex","cost-proc"))
+		#writer.writerow(("round","player","invest.","quantity","quality","q-cybex","q-firm","av-quality","av-risk","cost-cybex","cost-proc","val-cybex","val-mydata"))
+		writer.writerow(("round","invest.","quantity","quality","val-cybex","val-mydata"))
 		writer.writerows(val_array)
 	csvFile.close()
 
