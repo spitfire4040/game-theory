327d93f3a192753884fc0e54f0dbf9148efbcb2d
spitfire4040 <spitfire4040@protonmail.com>
2019-04-17 08:28:03 -0700

    update

--- rotation.py
+++ rotation.py
@@ -8,84 +8,97 @@ import math
 
 players = [0,1,2,3,4,5,6,7,8,9]
 
-def rotation():
+def calculate(I,s,q):
+
 	for x in range(0,10):
 		for player in players:
-			simulate(player)
-		print('*******************')
 
-def simulate(player):
-	result = calculate(I,s,q,player)
+			# initialize variables
+			Sc = 0.0    # set initial value for Sc
+			Sf = 0.0    # set initial value for Sf
+			x = 0.01   	# set cost per GB for processing
+			Q = 0.0     # set initial value for Q
+			R = 0.0		# set initial value for R
+			Ra = 0.0    # set initial value for Ra
+			r = 0.0     # set initial value for r
+			a = 1		# scaling factor for sharing utility
+			b = 1		# scaling factor for value
+			c = 1		# cost charged by CYBEX for participation
 
-def calculate(I,s,q,player):
-	# initialize variables
-	Sc = 0.0    # set initial value for Sc
-	Sf = 0.0    # set initial value for Sf
-	x = 0.01   	# set cost per GB for processing
-	Q = 0.0     # set initial value for Q
-	R = 0.0		# set initial value for R
-	Ra = 0.0    # set initial value for Ra
-	r = 0.0     # set initial value for r
-	a = 1		# scaling factor for sharing utility
-	b = 1		# scaling factor for value
-	c = 1		# cost charged by CYBEX for participation
+			# open file and insert previous values
+			if not os.path.isfile('/home/jay/Desktop/cybex.txt'):
+				pass
+			else:
+				with open('/home/jay/Desktop/cybex.txt', 'r') as infile:
+					input_list = []
+					for line in infile:
+						input_list.append(line)
 
-	# open file and insert previous values
-	if not os.path.isfile('/home/jay/Desktop/cybex.txt'):
-		continue
-	else:
-		with open('/home/jay/Desktop/cybex.txt', 'r') as infile:
-			input_list = []
-			for line in infile:
-				input_list.append(line)
+					Q = float(input_list[0])
+					Sc = float(input_list[1])
 
-			Q = float(input_list[1])
-			Ra = float(input_list[2])
-			Sc = float(input_list[3])
-			Sf = float(input_list[4])
+			if not os.path.isfile('/home/jay/Desktop/' + str(player) + '.txt'):
+				pass
+			else:
+				with open('/home/jay/Desktop/' + str(player) + '.txt', 'r') as infile:
+					input_list = []
+					for line in infile:
+						input_list.append(line)
 
-	# update average quality of data in CYBEX
-	Q = ((Q * Sc) + (q * s))/(Sc + s)
+					Ra = float(input_list[0])
+					Sf = float(input_list[1])
 
-	# calculate risk multiplier for firm based on investment level and amount of data shared
-	r1 = (1 / I)
-	r2 = (s * (1/q))
-	R = (r1 * r2)
+			# update average quality of data in CYBEX
+			Q = ((Q * Sc) + (q * s))/(Sc + s)
 
-	# update averate risk level for firm
-	Ra = ((Ra * Sf) + (R * s)) / (Sf + s)
+			# calculate risk multiplier for firm based on investment level and amount of data shared
+			r1 = (1 / I)
+			r2 = (s * (1/q))
+			R = (r1 * r2)
 
-	# processing cost for current transaction
-	X = (x * s)
+			# update averate risk level for firm
+			Ra = ((Ra * Sf) + (R * s)) / (Sf + s)
 
-	# add shared data to CYBEX running total
-	Sc += s
+			# processing cost for current transaction
+			X = (x * s)
 
-	# add shared data to firm's running total
-	Sf += s
+			# add shared data to CYBEX running total
+			Sc += s
 
-	# calculate current value of CYBEX data for sharing (for the firm)
-	V2 = (b * (math.log(1 + (Sc * Q)))) - Ra   # this is the value of the data in CYBEX plus my data
-	V1 = (b * (math.log(1 + (s * q)))) - Ra    # this is the value of my data only
+			# add shared data to firm's running total
+			Sf += s
 
-	# calculate sharing utility
-	if V2 - V1 < 0:
-		share = (a * math.log10(1 + I)) - x - c   # i'm the only one that has shared
-	else:
-		share = (V2 - V1) * (a * math.log10(1 + I)) - x - c   # value is the difference between CYBEX value and my value
+			# calculate current value of CYBEX data for sharing (for the firm)
+			V2 = (b * (math.log(1 + (Sc * Q)))) - Ra   # this is the value of the data in CYBEX plus my data
+			V1 = (b * (math.log(1 + (s * q)))) - Ra    # this is the value of my data only
 
-	# calculate not sharing utility
-	noshare = a * math.log10(1 + I)
+			# calculate sharing utility
+			if V2 - V1 < 0:
+				share = (a * math.log10(1 + I)) - x - c   # i'm the only one that has shared
+			else:
+				share = (V2 - V1) * (a * math.log10(1 + I)) - x - c   # value is the difference between CYBEX value and my value
 
-	# write current values out to file
-	with open('/home/jay/Desktop/cybex.txt','w') as outfile:
-		outfile.write(str(Q) + '\n')
-		outfile.write(str(Ra) + '\n')
-		outfile.write(str(Sc) + '\n')
-		outfile.write(str(Sf) + '\n')
+			# calculate not sharing utility
+			noshare = a * math.log10(1 + I)
+
+			# if share > noshare:
+			# 	print('Player-' + str(player) + ' chooses to share')
+			# else:
+			# 	print('Player-' + str(player) + ' chooses not to share')
+			print('player-' + str(player) + ' share = ',share)
+			print('player-' + str(player) + ' share = ',noshare)
+
+			# write current values out to file
+			with open('/home/jay/Desktop/cybex.txt','w') as outfile:
+				outfile.write(str(Q) + '\n')
+				outfile.write(str(Sc) + '\n')
+
+			with open('/home/jay/Desktop/firm-' + str(player) + '.txt', 'w') as outfile:
+				outfile.write(str(Ra) + '\n')
+				outfile.write(str(Sf) + '\n')
 
 def main(argv):
-	rotation()
+	calculate(1000,25,1)
 
 if __name__ == '__main__':
 	main(sys.argv)
